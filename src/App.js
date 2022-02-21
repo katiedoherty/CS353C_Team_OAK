8493038fb530a80ea8f75c30896888c77a8e1699
James Greer <88616420+James-Greer-Git@users.noreply.github.com>
2022-02-21 16:35:33 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header"></header>
-    </div>
-  );
+  return <div className="App">Hello</div>;
 }
 
 export default App;
