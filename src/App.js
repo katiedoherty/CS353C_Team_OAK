3f9db5a4946d22ed4c4065df8f325ebb80a94d9a
Owen Richards <owen.richards.ire@gmail.com>
2022-03-07 15:57:12 +0000

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import CardDisplay from "../src/components/cards/CardDisplay.js";
 
 function App() {
-  return <div className="App">Goodbye</div>;
+  return (
+    <div className="App">
+      <div className="Wrapper">
+        <CardDisplay />
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/cards/Card.js
@@ -0,0 +1,9 @@
+// import React from "react";
+// import cardTemp from "/src/icons/cardTemp.svg";
+
+// class Card extends React.Component {
+//   render() {
+//     return <img src={cardTemp} alt="Card" />;
+//   }
+// }
+// export default Card;
