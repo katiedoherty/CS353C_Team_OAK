768a577117016bb330cc90e13a51484535fad79e
Owen Richards <owen.richards.ire@gmail.com>
2022-05-20 15:37:33 +0100

    Added Card Hover Feature

--- src/components/CardDisplay/CardAnimation.js
+++ src/components/CardDisplay/CardAnimation.js
@@ -9,7 +9,8 @@ import axios from "axios";
 import RandomCardGenerator from "../randomCardGenerator/newCardGenerator";
 import { useParams } from "react-router-dom";
 import { useEffect } from "react";
-import Modalfunction from './Buttons/UserCardsButton'
+import Modalfunction from "./Buttons/UserCardsButton";
+
 const Container = styled.div`
   display: grid;
   max-width: calc(100% - 45px);
@@ -21,9 +22,7 @@ const Container = styled.div`
   }
 `;
 
-
 const StyledButton = styled.button`
-
   @import url("https://fonts.googleapis.com/css2?family=Bodoni+Moda&display=swap");
 
   border-radius: 12px;
@@ -61,18 +60,15 @@ const StyledButton = styled.button`
   }
 `;
 
-
-
-
 //const newCard = RandomCardGenerator();
 
 const CardAnimation = () => {
   //console.log(props.data);
   const params = useParams();
- 
+
   const useremail = params.id;
-  const [chicken , setChicken] = useState(0);
-  const [salmon, setSalmon]=useState(0);
+  const [chicken, setChicken] = useState(0);
+  const [salmon, setSalmon] = useState(0);
   const [turkey, setTurkey] = useState(0);
   const [steak, setSteak] = useState(0);
   const [tripchef, settripchef] = useState(0);
@@ -80,7 +76,7 @@ const CardAnimation = () => {
   const [complementsfood, setcomplementsfood] = useState(0);
   const [cookedtoperfection, setcookedtoperfection] = useState(0);
   const [sprouts, setSprouts] = useState(0);
-  const [peas, setPeas] =  useState(0);
+  const [peas, setPeas] = useState(0);
   const [carrots, setCarrots] = useState(0);
   const [broccoli, setBroccoli] = useState(0);
   const [patatoes, setPotatoes] = useState(0);
@@ -101,10 +97,9 @@ const CardAnimation = () => {
     },
   ]);
 
-  useEffect(()=>{
+  useEffect(() => {
     setItems([]);
-   
-  },[])
+  }, []);
   const addItem = () => {
     const timestamp = Date.now();
     setItems([
@@ -117,142 +112,109 @@ const CardAnimation = () => {
       },
     ]);
     RandomCardGenerator();
-    var elts =document.getElementsByClassName("usercards");
+    var elts = document.getElementsByClassName("usercards");
     for (var i = 0; i < elts.length; ++i) {
       console.log(elts[i].value);
-  }
+    }
   };
 
-
- 
   const userCards = async () => {
-  
-    
-      var updatecards = [];
-      var elts =document.getElementsByClassName("usercards");
-      for (var i = 0; i < elts.length; ++i) {
-        updatecards.push(elts[i].value);
-  
-        if(updatecards[i]=="Chicken"){
-          setChicken((prevValue) => prevValue + 1)
-        }
-        else if(updatecards[i]=="Salmon"){
-         setSalmon((prevValue) => prevValue + 1)
-       }
-       else if(updatecards[i]=="Turkey"){
-         setTurkey((prevValue) => prevValue + 1)
-       }
-       else if(updatecards[i]=="Steak"){
-         setSteak((prevValue) => prevValue + 1)
-       }
-       else if(updatecards[i]=="Trip the chef"){
-        settripchef((prevValue) => prevValue + 1)
-      }
-      else if(updatecards[i]=="Food on the Floor!"){
-        setpushfood((prevValue) => prevValue + 1)
-      }
-      else if(updatecards[i]=="Ramsay Complements!"){
-        setcomplementsfood((prevValue) => prevValue + 1)
-      }
-      else if(updatecards[i]=="Cooked to Perfection!"){
-        setcookedtoperfection((prevValue) => prevValue + 1)
-      }
-      else if(updatecards[i]=="Sprouts"){
-        setSprouts((prevValue) => prevValue + 1)
-      }
-      else if(updatecards[i]=="Peas"){
-        setPeas((prevValue) => prevValue + 1)
-      }
-      else if(updatecards[i]=="Carrots"){
-        setCarrots((prevValue) => prevValue + 1)
-      }
-      else if(updatecards[i]=="Broccoli"){
-        setBroccoli((prevValue) => prevValue + 1)
-      }
-      else if(updatecards[i]=="Potatoes"){
-        setPotatoes((prevValue) => prevValue + 1)
-      }
-      else if(updatecards[i]=="Chips"){
-        setChips((prevValue) => prevValue + 1)
-      }
-      else if(updatecards[i]=="Pasta"){
-        setPasta((prevValue) => prevValue + 1)
-      }
-      else if(updatecards[i]=="Rice"){
-        setRice((prevValue) => prevValue + 1)
-      }
-      else if(updatecards[i]=="Salt & Pepper"){
-        setSaltandpepper((prevValue) => prevValue + 1)
-      }
-      else if(updatecards[i]=="Ketchup"){
-        setKetchup((prevValue) => prevValue + 1)
-      }
-      else if(updatecards[i]=="Gravy"){
-        setGravy((prevValue) => prevValue + 1)
-      }
-      else if(updatecards[i]=="Saffron"){
-        setSaffron((prevValue) => prevValue + 1)
-      }
-      
+    var updatecards = [];
+    var elts = document.getElementsByClassName("usercards");
+    for (var i = 0; i < elts.length; ++i) {
+      updatecards.push(elts[i].value);
 
+      if (updatecards[i] == "Chicken") {
+        setChicken((prevValue) => prevValue + 1);
+      } else if (updatecards[i] == "Salmon") {
+        setSalmon((prevValue) => prevValue + 1);
+      } else if (updatecards[i] == "Turkey") {
+        setTurkey((prevValue) => prevValue + 1);
+      } else if (updatecards[i] == "Steak") {
+        setSteak((prevValue) => prevValue + 1);
+      } else if (updatecards[i] == "Trip the chef") {
+        settripchef((prevValue) => prevValue + 1);
+      } else if (updatecards[i] == "Food on the Floor!") {
+        setpushfood((prevValue) => prevValue + 1);
+      } else if (updatecards[i] == "Ramsay Complements!") {
+        setcomplementsfood((prevValue) => prevValue + 1);
+      } else if (updatecards[i] == "Cooked to Perfection!") {
+        setcookedtoperfection((prevValue) => prevValue + 1);
+      } else if (updatecards[i] == "Sprouts") {
+        setSprouts((prevValue) => prevValue + 1);
+      } else if (updatecards[i] == "Peas") {
+        setPeas((prevValue) => prevValue + 1);
+      } else if (updatecards[i] == "Carrots") {
+        setCarrots((prevValue) => prevValue + 1);
+      } else if (updatecards[i] == "Broccoli") {
+        setBroccoli((prevValue) => prevValue + 1);
+      } else if (updatecards[i] == "Potatoes") {
+        setPotatoes((prevValue) => prevValue + 1);
+      } else if (updatecards[i] == "Chips") {
+        setChips((prevValue) => prevValue + 1);
+      } else if (updatecards[i] == "Pasta") {
+        setPasta((prevValue) => prevValue + 1);
+      } else if (updatecards[i] == "Rice") {
+        setRice((prevValue) => prevValue + 1);
+      } else if (updatecards[i] == "Salt & Pepper") {
+        setSaltandpepper((prevValue) => prevValue + 1);
+      } else if (updatecards[i] == "Ketchup") {
+        setKetchup((prevValue) => prevValue + 1);
+      } else if (updatecards[i] == "Gravy") {
+        setGravy((prevValue) => prevValue + 1);
+      } else if (updatecards[i] == "Saffron") {
+        setSaffron((prevValue) => prevValue + 1);
+      }
     }
 
-var cards = document.querySelectorAll(`.cardDisplay`);
+    var cards = document.querySelectorAll(`.cardDisplay`);
 
-for(var i =0; i<items.length; i++){
- 
- setItems([]);
-}
+    for (var i = 0; i < items.length; i++) {
+      setItems([]);
+    }
 
+    updatecards = [];
+  };
 
-updatecards = [];
-
-
-  }
-
-   const update = async() => {
+  const update = async () => {
     const res = await axios.put(`http://localhost:8080/api/usercards`, {
       email: useremail,
-      Protein:{
-        Chicken:chicken,
-        Salmon:salmon,
-        Turkey:turkey,
-        Steak:steak
+      Protein: {
+        Chicken: chicken,
+        Salmon: salmon,
+        Turkey: turkey,
+        Steak: steak,
       },
-      WildCards:{
-       
-        Tripthechef:tripchef,
-        pushyouropponentsfoodoffthetable:pushfood,
-        Ramsaycomplementsyourfood:complementsfood,
-        Cookedtoperfection:cookedtoperfection
-    },
-    Vegetables:{
-       
-      Sprouts:sprouts,
-      Peas:peas,
-      Carrots:carrots,
-      Broccoli:broccoli
-  },
-  Carbs:{
-       
-    Patatoes:patatoes,
-    Chips:chips,
-    Pasta:pasta,
-    Rice:rice
-},
-Modifiers:{
-       
-  SaltAndPepper:saltandpepper,
-  Ketchup:ketchup,
-  Gravy:gravy,
-  Saffron:saffron
-}
-    })
-   
-  console.log(res.data);
-   }
+      WildCards: {
+        Tripthechef: tripchef,
+        pushyouropponentsfoodoffthetable: pushfood,
+        Ramsaycomplementsyourfood: complementsfood,
+        Cookedtoperfection: cookedtoperfection,
+      },
+      Vegetables: {
+        Sprouts: sprouts,
+        Peas: peas,
+        Carrots: carrots,
+        Broccoli: broccoli,
+      },
+      Carbs: {
+        Patatoes: patatoes,
+        Chips: chips,
+        Pasta: pasta,
+        Rice: rice,
+      },
+      Modifiers: {
+        SaltAndPepper: saltandpepper,
+        Ketchup: ketchup,
+        Gravy: gravy,
+        Saffron: saffron,
+      },
+    });
 
-   useEffect(()=>{
+    console.log(res.data);
+  };
+
+  useEffect(() => {
     update();
     setChicken(0);
     setSalmon(0);
@@ -270,10 +232,20 @@ Modifiers:{
     setKetchup(0);
     setGravy(0);
     setSaffron(0);
-  },[chicken, steak, salmon, turkey, tripchef, pushfood, complementsfood, cookedtoperfection, ketchup, saltandpepper, gravy, saffron])
-  
-
-
+  }, [
+    chicken,
+    steak,
+    salmon,
+    turkey,
+    tripchef,
+    pushfood,
+    complementsfood,
+    cookedtoperfection,
+    ketchup,
+    saltandpepper,
+    gravy,
+    saffron,
+  ]);
 
   const removeItem = (id) => {
     console.log("The id of this item:" + id);
@@ -301,15 +273,20 @@ Modifiers:{
         </Container>
       </div>
       <div className="buttonContainer">
-        <IngredientsButton userCards = {userCards} label={"Add To Deck"}></IngredientsButton>
+        <IngredientsButton
+          userCards={userCards}
+          label={"Add To Deck"}
+        ></IngredientsButton>
         <UnpackButton label={"Unpack Cards"} addItem={addItem}></UnpackButton>
         <StyledButton
-       
-  className ="openModalBtn"
-  onClick={()=>{
-    setOpenModal(true)
-  }}>Your Cards</StyledButton>
-  {openModal && <Modalfunction close ={setOpenModal} />}
+          className="openModalBtn"
+          onClick={() => {
+            setOpenModal(true);
+          }}
+        >
+          Your Cards
+        </StyledButton>
+        {openModal && <Modalfunction close={setOpenModal} />}
         <LogOutButton
           label={"Log Out"}
           handleLogout={handleLogout}
--- src/components/CardDisplay/PlayingCard.js
+++ src/components/CardDisplay/PlayingCard.js
@@ -1,13 +1,10 @@
 import React from "react";
 import styled from "styled-components";
 
-import Card from "@mui/material/Paper";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-
 const StyledCard = styled.div`
+  @import url("https://fonts.googleapis.com/css2?family=Bodoni+Moda&display=swap");
   margin: 0px;
-  
+
   .card-wrapper {
     position: relative;
   }
@@ -18,20 +15,42 @@ const StyledCard = styled.div`
     height: 280px;
     width: 200px;
     padding: 8px;
-    );
   }
-  
-  .gener_deple{
+
+  .gener_deple {
     display: none;
-    transition: .2s;
+    transition: 0.2s;
     transform: translateY(1em);
   }
-
-  .inputButton_usercards:hover +  .gener_deple{
-    display: block;
-    transform: translateY(0);
+  .cardImage {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
   }
 
+  .cardDisplay:hover {
+    background-color: yellow;
+  }
+
+  .cardDisplay:hover .gener_deple {
+    display: block;
+    transition: 0.2s;
+    transform: translateY(-10em);
+  }
+  .inputButton_usercards {
+    width: 100%;
+    height: 10%;
+    text-align: center;
+    border: 2px solid #111;
+    background-color: white;
+    font-family: "Bodoni Moda", serif;
+    font-size: 16px;
+    font-weight: bold;
+    padding: 1px;
+    color: black;
+    padding: 1em;
+    line-height: 0.5rem;
+  }
 `;
 
 const PlayingCard = (props) => {
@@ -39,36 +58,35 @@ const PlayingCard = (props) => {
     <StyledCard key={props.id} onClick={props.removeItem}>
       <div className="cardDisplay">
         <img
-        className="cardImage"
+          className="cardImage"
           id="cardImage"
           src={require("../Images/icons/Card5.jpg")}
           alt="card"
         />
-        <input
-          className="inputButton_usercards"
-          type="text"
-          id="randomCard"
-          name="randomCard"
-          readOnly="readOnly"
-        />
-        
         <div className="gener_deple">
-        <input
-          className="input_generation"
-          type="text"
-          id="Cardgeneration"
-          name="Cardgeneration"
-          readOnly="readOnly"
-        />
+          <input
+            className="inputButton_usercards"
+            type="text"
+            id="randomCard"
+            name="randomCard"
+            readOnly="readOnly"
+          />
+          <input
+            className="inputButton_usercards"
+            type="text"
+            id="Cardgeneration"
+            name="Cardgeneration"
+            readOnly="readOnly"
+          />
 
-        <input
-          className="input_depletion"
-          type="text"
-          id="CardDepletion"
-          name="CardDepletion"
-          readOnly="readOnly"
-        />
-      </div>
+          <input
+            className="inputButton_usercards"
+            type="text"
+            id="CardDepletion"
+            name="CardDepletion"
+            readOnly="readOnly"
+          />
+        </div>
       </div>
     </StyledCard>
   );
